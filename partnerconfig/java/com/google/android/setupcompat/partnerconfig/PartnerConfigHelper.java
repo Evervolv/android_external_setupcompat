a3a337cd6a6f0d83053ae32b256f2ea00c52abbb
Michael Bestas <mkbestas@lineageos.org>
2024-02-16 16:01:35 +0200

    Change some more defaults
    Change-Id: Ia80cbc67f85bd8577262c16ecc2c447962ff1db2

--- partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
+++ partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
@@ -1018,9 +1018,9 @@ public class PartnerConfigHelper {
           return shouldApplyExtendedPartnerConfig(context);
         }
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard Material You configs supporting status unknown; return as false.");
+        Log.w(TAG, "SetupWizard Material You configs supporting status unknown; return as true.");
         applyMaterialYouConfigBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -1046,9 +1046,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard default theme status unknown; return as null.");
+        Log.w(TAG, "SetupWizard default theme status unknown; return as glif_v4.");
         suwDefaultThemeBundle = null;
-        return null;
+        return "glif_v4";
       }
     }
     if (suwDefaultThemeBundle == null || suwDefaultThemeBundle.isEmpty()) {
@@ -1093,9 +1093,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard full dynamic color supporting status unknown; return as false.");
+        Log.w(TAG, "SetupWizard full dynamic color supporting status unknown; return as true.");
         applyFullDynamicColorBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -1118,9 +1118,9 @@ public class PartnerConfigHelper {
       } catch (IllegalArgumentException | SecurityException exception) {
         Log.w(
             TAG,
-            "SetupWizard one-pane support in embedded activity status unknown; return as false.");
+            "SetupWizard one-pane support in embedded activity status unknown; return as true.");
         applyEmbeddedActivityOnePaneBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -1142,9 +1142,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "Neutral button style supporting status unknown; return as false.");
+        Log.w(TAG, "Neutral button style supporting status unknown; return as true.");
         applyNeutralButtonStyleBundle = null;
-        return false;
+        return true;
       }
     }
 
@@ -1165,9 +1165,9 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "Font weight supporting status unknown; return as false.");
+        Log.w(TAG, "Font weight supporting status unknown; return as true.");
         applyFontWeightBundle = null;
-        return false;
+        return true;
       }
     }
 
